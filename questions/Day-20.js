f15d43b3bdff61c1d2f8965bf991187751849d3e
Parth Patel <parthpatel4042@gmail.com>
2023-10-10 09:31:13 +0530

    Day 20 Solution

---
+++ questions/Day-20.js
@@ -0,0 +1,21 @@
+// Code with Parth
+
+var x = 5;
+var x = 7;
+
+console.log(x);
+
+// 👍A) 5
+// 👏B) 7
+// 💡C) SyntaxError
+// 😄D) ReferenceError
+
+// Answer: B
+
+// Explanation: The var keyword is used to declare a variable x and assign it the value 5.
+
+// Then, the var keyword is used again to declare a variable x and assign it the value 7. This does not result in any errors; it simply reassigns the variable x.
+
+// Finally, console.log(x) prints the value of x, which is 7 because the second assignment overwrites the first one.
+
+// So, the correct answer is Option B: 7.
