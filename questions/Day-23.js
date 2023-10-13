79ecb4905d29ae83a885ce1eeaedd8e4820e4499
Parth Patel <parthpatel4042@gmail.com>
2023-10-13 08:43:07 +0530

    Day 23 Solution

---
+++ questions/Day-23.js
@@ -0,0 +1,15 @@
+// Code with Parth
+
+for (let i = 1; i < 5; i++) {
+  if (i === 3) continue;
+  console.log(i);
+}
+
+// 👍A) 1 2
+// 👏B) 1 2 3
+// 💡C) 1 2 4
+// 😄D) 1 3 4 
+
+// Answer: C
+
+// Explanation: The continue statement skips an iteration if a certain condition returns true.
