c5bf859616cddd4a6a62d991cf307d454a956dc7
Parth Patel <parthpatel4042@gmail.com>
2023-10-21 11:13:04 +0530

    Day 31 Solution

---
+++ questions/Day-31.js
@@ -0,0 +1,27 @@
+// Code with Parth
+
+console.log(0);
+console.log(new String('Hello'));
+console.log('');
+console.log(' ');
+console.log(new Boolean(true));
+console.log(null);
+
+// 👍A) 0, [object String], <empty line>, <space>, [object Boolean], null
+// 👏B) 0, new String('Hello'), '', new Boolean(true), null
+// 💡C) 0, '', new Boolean(true), null
+// 😄D) All of them are falsy
+
+// Answer: A
+
+// Explanation: console.log(0); - This logs the number 0 to the console.
+
+// console.log(new String('Hello')); - This logs an object of type String to the console. The output will look something like [object String].
+
+// console.log(''); - This logs an empty string to the console. It appears as an empty line in the console output.
+
+// console.log(' '); - This logs a string with a single space to the console. It prints a space in the console output.
+
+// console.log(new Boolean(true)); - This logs an object of type Boolean to the console. The output will look something like [object Boolean].
+
+// console.log(null); - This logs the value null to the console.
