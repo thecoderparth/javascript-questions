676b3e1935706743432a337afa90d6efaf5956e9
Parth Patel <parthpatel4042@gmail.com>
2023-10-22 11:18:53 +0530

    Day 32 Solution

---
+++ questions/Day-32.js
@@ -0,0 +1,12 @@
+// Code with Parth
+
+console.log(typeof typeof 1);
+
+// 👍A) "number"
+// 👏B) "string"
+// 💡C) "object"
+// 😄D) "undefined"
+
+// Answer: B
+
+// Explanation: typeof 1 returns "number". typeof "number" returns "string"
