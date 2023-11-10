f8f6e5ea7e9be6d5c011e6afa2caa5519cf3e53e
Parth Patel <parthpatel4042@gmail.com>
2023-11-10 09:26:47 +0530

    Day 51 Solution

---
+++ questions/Day-51.js
@@ -0,0 +1,19 @@
+// Code with Parth
+
+const colors = ['red', 'blue', 'green', 'yellow'];
+const [z] = colors;
+
+console.log(z);
+
+// 👍A) 'red'
+// 👏B) 'blue'
+// 💡C) 'green'
+// 😄D) 'yellow'
+
+// Answer: A
+
+// Explanation: There is an array colors containing four string elements.
+
+// The destructuring assignment [z] is used to extract the first element of the array and assign it to the variable z.
+
+// Therefore, z will hold the value of the first element of the array, which is 'red'.
