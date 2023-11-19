0a03beb591ff91e6a167e54c799709cc903d6fbe
Parth Patel <parthpatel4042@gmail.com>
2023-11-19 10:33:44 +0530

    Day 60 Solution

---
+++ questions/Day-60.js
@@ -0,0 +1,27 @@
+// Code with Parth
+
+const values = [3 - 1, 4 % 2, 2 ** 2];
+console.log(values);
+
+// 👍A) [2, 0, 4]
+// 👏B) [3, 0, 4]
+// 💡C) [2, 1, 2]
+// 😄D) [1, 0, 2]
+
+// Answer: A
+
+// Explanation: An array is created with three elements.
+// Each element is computed using mathematical operations.
+// In the original code:
+
+// 1 + 2 evaluates to 3.
+// 1 * 2 evaluates to 2.
+// 1 / 2 evaluates to 0.5.
+// So, the original code logs [3, 2, 0.5] to the console.
+
+// In the similar question:
+
+// 3 - 1 evaluates to 2.
+// 4 % 2 evaluates to 0.
+// 2 ** 2 evaluates to 4.
+// So, the correct answer is Option A: [2, 0, 4].
